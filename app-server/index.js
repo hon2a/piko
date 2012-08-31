972c2407a58b5a3cef061213b90381ca22524a49
hon2a <jkonopasek@koukaam.se>
2012-08-31 11:41:09 +0200

    Initialized repository with the base of the Node.js server application.

---
+++ app-server/index.js
@@ -0,0 +1,49 @@
+var config = require('./config.json'),
+	express = require('express'),
+	resource = require('express-resource'),
+	fn = require('functions'),
+	database = require('database'),
+	User = require('User');
+
+var finisher = function (req, res, next) {
+		res.finish = function (err, data) {
+			if (err) {
+				res.send(err, HTTP.INTERNAL_SERVER_ERROR);
+			} else if (data) {
+				res.send(data);
+			} else {
+				res.send(HTTP.OK);
+			}
+		};
+	},
+	db = new database.Db({
+		factory: require('mysql'),
+		config: config.db
+	}),
+	app = module.exports = express.createServer();
+
+app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session(config.session));
+
+app.use(express.static(__dirname + '/../app-client'));
+
+app.all('/api/*', finisher, db.feeder(), User.access);
+
+app.get('/api/session/login', User.login);
+app.get('/api/session/logout', User.logout);
+app.get('/api/session', User.get);
+
+app.resource('api/usertypes', require('resources/usertypes'));
+app.resource('api/users', require('resources/users'));
+
+db.initialize(function (err) {
+	if (err) {
+		throw err;
+	}
+	
+	app.listen(8888);
+	console.log('App started. Listening on port 8888...');
+});
